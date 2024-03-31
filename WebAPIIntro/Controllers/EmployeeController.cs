b5a412116264846b5930520e0630d86eb64be46b
Ahmet Mestan KAHYA <ahmtmkhya@hotmail.com>
2024-03-31 12:29:03 +0300

    Put / Delete endpoints added

--- WebAPIIntro/Controllers/EmployeeController.cs
+++ WebAPIIntro/Controllers/EmployeeController.cs
@@ -29,5 +29,52 @@ namespace WebAPIIntro.Controllers
 
             return Ok(employee);
         }
+
+        [HttpPost]
+        public IActionResult PostEmployee(Employee employee)
+        {
+            NorthwndContext db = new NorthwndContext();
+            db.Employees.Add(employee);
+            db.SaveChanges();
+
+            return Ok("Employe ekleem işlemi başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteEmployee(int id)
+        {
+            NorthwndContext db = new NorthwndContext();
+            Employee employee = db.Employees.Find(id);
+
+            if (employee == null)
+            {
+                return NotFound("Böyle bir Employee bulunamamıştır");
+            }
+
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return Ok("Employee başarı ile silinmiştir");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateEmployee(Employee employee)
+        {
+            NorthwndContext db = new NorthwndContext();
+            Employee entity = db.Employees.FirstOrDefault(x=>x.EmployeeId== employee.EmployeeId);
+
+            if (entity == null)
+            {
+                return NotFound("Böyle bir employee bulunamamıştır");
+            }
+
+            entity.FirstName= employee.FirstName;
+            entity.LastName= employee.LastName;
+            entity.Title= employee.Title;
+            db.SaveChanges();
+
+            return Ok("Güncelleme başarı ile yapılmıştır");
+
+
+        }
     }
 }
--- WebAPIIntro/Controllers/SupplierController.cs
+++ WebAPIIntro/Controllers/SupplierController.cs
@@ -28,5 +28,49 @@ namespace WebAPIIntro.Controllers
             }
             return Ok(supplier);
         }
+
+        [HttpPost]
+        public IActionResult PostSupplier(Supplier supplier)
+        {
+            NorthwndContext db = new NorthwndContext();
+            db.Suppliers.Add(supplier);
+            db.SaveChanges();
+
+            return Ok("Ekleme işlmei başarılı");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            NorthwndContext db = new NorthwndContext();
+            Supplier supplier = db.Suppliers.FirstOrDefault(x=>x.SupplierId== id);
+
+            if(supplier == null)
+            {
+                return NotFound();
+            }
+
+            db.Suppliers.Remove(supplier);
+            db.SaveChanges();
+
+            return Ok("Silme işlemi başarılı");
+
+        }
+
+        [HttpPut]
+        public IActionResult PutSupplier(Supplier supplier)
+        {
+            NorthwndContext db = new NorthwndContext();
+            Supplier entitiy = db.Suppliers.FirstOrDefault(x=>x.SupplierId == supplier.SupplierId);
+
+            entitiy.CompanyName = supplier.CompanyName;
+            entitiy.ContactName = supplier.ContactName;
+            entitiy.ContactTitle = supplier.ContactTitle;
+
+            db.SaveChanges();
+
+            return Ok("Güncelleme işlemi başarılı");
+        }
+
     }
 }
